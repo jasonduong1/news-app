d25fbdadc9b14a92633fcb5397768664ec3e10f7
jason <jduong89@gmail.com>
2022-03-29 18:20:45 -0700

    changes index action to take in keyword params

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,6 @@
 class ArticlesController < ApplicationController
   def index
-    response = HTTP.get("https://newsapi.org/v2/everything?q=bitcoin&apiKey=#{Rails.application.credentials.news_api.api_key}")
+    response = HTTP.get("https://newsapi.org/v2/everything?q=#{params[:keyword]}&apiKey=#{Rails.application.credentials.news_api.api_key}")
     render json: response.parse(:json)
   end
 end
