b99fb57217b0deeacfc728e8b7509e19b41fa2d1
jason <jduong89@gmail.com>
2022-03-29 18:17:17 -0700

    changes index action to link to news api

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,6 @@
 class ArticlesController < ApplicationController
   def index
-    render json: { message: "hey!" }
+    response = HTTP.get("https://newsapi.org/v2/everything?q=bitcoin&apiKey=#{Rails.application.credentials.news_api.api_key}")
+    render json: response.parse(:json)
   end
 end
